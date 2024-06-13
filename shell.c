670abdf9202300c3cef116cc8f8142f6ec577c87
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-06-14 03:56:33 +0900

    wip

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <stdnoreturn.h>
 
-noreturn static void execute(FILE* f);
+noreturn static void execute(int);
 
 int main(int argc, char *argv[]) {
     pid_t pid;
@@ -18,16 +18,10 @@ int main(int argc, char *argv[]) {
         err_sys("fork error");
     } else if (pid == 0) { // child
         close(fd[0]);
-        FILE* f = fdopen(fd[1], "w");
-        if (f == NULL) {
-            err_sys("fdopen error");
-        }
-        execute(f);
+        execute(fd[1]);
     }
 
     // parent
-    if ((pid = waitpid(pid, &status, 0)) < 0)
-        err_sys("waitpid error");
     close(fd[1]);
     FILE *f = fdopen(fd[0], "r");
     if (f == NULL)
@@ -35,21 +29,24 @@ int main(int argc, char *argv[]) {
     for(int c; (c = fgetc(f)) != EOF;) {
         fputc(c, stdout);
     }
+
+    if ((pid = waitpid(pid, &status, 0)) < 0)
+        err_sys("waitpid error");
     return 0;
 }
 
-noreturn static void execute(FILE *f)
+noreturn static void execute(int fd)
 {
     char exec_file[BUFSIZ];
     char arg1[BUFSIZ];
 
-    dup2(fileno(f), fileno(stdout));
+    dup2(fd, fileno(stdout));
 
     strcpy(exec_file, "date");
     strcpy(arg1, "-R");
     execlp(exec_file, exec_file, arg1, (char *)0);
 
-    fclose(f);
+    close(fd);
     err_ret("couldn't execute: %s", exec_file);
     exit(127);
 }
