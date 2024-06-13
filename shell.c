113d95dc88ec12537c03098fd19e08fed642a2b7
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-06-14 02:18:48 +0900

    fork

--- shell.c
+++ shell.c
@@ -4,22 +4,32 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    char exec_file[BUFSIZ];
-    char arg1[BUFSIZ];
+    pid_t pid;
+    int status;
+    
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) { // child
+        char exec_file[BUFSIZ];
+        char arg1[BUFSIZ];
 
-    char* filename = "output";
-    FILE* f = fopen(filename, "w");
-    if (!f) {
-        err_ret("couldn't open file: %s", filename);
+        char* filename = "output";
+        FILE* f = fopen(filename, "w");
+        if (!f) {
+            err_ret("couldn't open file: %s", filename);
+        }
+        dup2(fileno(f), fileno(stdout));
+
+        strcpy(exec_file, "date");
+        strcpy(arg1, "-R");
+        execlp(exec_file, exec_file, arg1, (char *)0);
+        fclose(f);
+        err_ret("couldn't execute: %s", exec_file);
+        exit(127);
     }
-    dup2(fileno(f), fileno(stdout));
 
-    strcpy(exec_file, "date");
-    strcpy(arg1, "-R");
-    execlp(exec_file, exec_file, arg1, (char *)0);
-    fclose(f);
-    err_ret("couldn't execute: %s", exec_file);
-    exit(127);
-
+    // parent
+    if ((pid = waitpid(pid, &status, 0)) < 0)
+        err_sys("waitpid error");
     return 0;
 }
